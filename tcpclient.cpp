44ba646222b9f4c4a5a7aa682b20a756536caa4d
ztcode9547 <3343267498@qq.com>
2024-10-04 17:15:17 +0800

    第一次成功连接上服务器” git commit -m 第一次成功连接上服务器”

---
+++ tcpclient.cpp
@@ -0,0 +1,6 @@
+#include "tcpclient.h"
+
+tcpclient::tcpclient()
+{
+
+}
---
+++ tcpclient.h
@@ -0,0 +1,12 @@
+#ifndef TCPCLIENT_H
+#define TCPCLIENT_H
+
+#include <QObject>
+
+class tcpclient
+{
+public:
+    tcpclient();
+};
+
+#endif // TCPCLIENT_H
